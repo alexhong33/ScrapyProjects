61615c80677ed23490c9ed9ea8653c471605c373
alexhong33 <alexhongf@163.com>
2017-07-14 17:46:10 +0800

    微信爬虫+代理

---
+++ basic/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# @Time        : 2017/7/14 15:57
+# @Author      : Hong
+# @Contact     : alexhongf@163.com
+# @File        : __init__.py.py
+# @Description : STOP wishing START doing. 
